693272543074f320b3c5ea3a1547675c96c118e0
ZheXing1016 <zhexing1016@qq.com>
2018-02-28 23:24:32 +0800

    初始化
    完成基础初始化，并且可以插入和遍历所有的数据

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_sqliteLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new DBContext();
+            //db.Students.Add(new students { SNAME = "姚", SEX = "女", AGE = 21 });
+            //var count = db.SaveChanges();
+
+            foreach(var table in db.Students)
+            {
+                Console.WriteLine($"SID={table.SID}|SNAME={table.SNAME}|SEX={table.SEX}|AGE={table.AGE}|");
+            }
+
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ dbmodule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EF_sqliteLearn
+{
+
+    public class DBContext:DbContext
+    {
+        public DbSet<students> Students { set; get; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={System.IO.Directory.GetCurrentDirectory()}\\db.db;");
+            //optionsBuilder.UseSqlite($"Data Source=db.db;");
+        }
+    }
+    [Table("students")]
+    public class students
+    {
+        [Key]
+        public int SID { set; get; }
+        public string SNAME { set; get; }
+        public string SEX { set; get; }
+        public int AGE { set; get; }
+    }
+    class dbmodule
+    {
+    }
+}
